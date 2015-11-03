cd3d178b2c624039c6a831d7a645c96183c0ed92
Your Name <you@example.com>
2015-11-03 13:12:28 +0100

    pobieranie pakietu z karty sieciowej

--- src/PacketAnalyzer.c
+++ src/PacketAnalyzer.c
@@ -11,6 +11,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+
 #include "naglowki.h"
 #include "funkcje.h"
 
@@ -52,39 +57,121 @@ int main(void) {
 
 	};
 
+	//definicja zmiennych
+	int s; /*deskryptor gniazda*/
+	int j;
+	int i = 0;
+	int length = 0;
 
-	eth_arp_packet ARP;
-	unsigned int arp_size = sizeof(eth_arp_packet);
-	convert_raw_to_arp_packet(&ARP,&bufor_eth_arp,arp_size);
-	print_arp(ARP);
-
-
-
-
-	eth_ip_icmp_packet ICMP;
-	unsigned int icmp_size = sizeof(eth_ip_icmp_packet);
-	convert_raw_to_icmp_packet(&ICMP,&bufor_eth_ip_icmp,icmp_size);
-	print_icmp(ICMP);
-
+	//bufor dla ramek z Ethernetu
+	void* buffer = (void*)malloc(ETH_FRAME_LEN);
+	//wskaznik do naglowka Eth
+	unsigned char* etherhead = buffer;
+	//wskaznik do miejsca rozpoczecia danych
+	unsigned char* data = buffer + 14;
 
 
-	eth_ip_udp_packet UDP;
-	unsigned int udp_size = sizeof(eth_ip_udp_packet);
-	convert_raw_to_udp_packet(&UDP,&bufor_eth_ip_udp,sizeof(bufor_eth_ip_udp));
-	print_udp(UDP);
+	s = socket(AF_INET, SOCK_PACKET, htons(ETH_P_ALL));
+	if (s == -1) {printf ("Nie moge otworzyc gniazda\n");}
+
+	while (i<1) {
+			//odbierz ramke Eth
+			length = recvfrom(s, buffer, ETH_FRAME_LEN, 0, NULL, NULL);
+			if (length == -1)
+				printf ("Problem z odbiorem ramki \n");
+			else {
+				i++;
+				printf ("Ramka: %d, dlugosc: %d [B]\n", i, length);
+			}
+
+			#if 1
+			//wypisz zawartosc bufora
+				for (j=0;j<length; j++) {
+					printf ("%02x ", *(etherhead+j));
+				}
+				printf ("\n");
+			#endif
+	}
+
+	ethernet2_frame et2;
+	memcpy(&et2,etherhead,14);
+
+	short type = et2.frame_type[0]<<8 | et2.frame_type[1];
+
+	switch (type) {
+		case 0x0800:
+			printf("IPv4 \n");
+			ip_frame ip4;
+			memcpy(&ip4,etherhead+14,20);
+
+			switch(ip4.protocol){
+				case 0x06:
+					printf("Ip Protocol Type : TCP\n");
+					eth_ip_tcp_packet TCP;
+					unsigned int tcp_size = sizeof(eth_ip_tcp_packet);
+					convert_raw_to_tcp_packet(&TCP,&bufor_eth_ip_tcp,tcp_size);
+					print_tcp(TCP);
+					break;
+				case 0x01:
+					printf("Ip Protocol Type : ICMP\n");
+					eth_ip_icmp_packet ICMP;
+					unsigned int icmp_size = sizeof(eth_ip_icmp_packet);
+					convert_raw_to_icmp_packet(&ICMP,&etherhead,icmp_size);
+					print_icmp(ICMP);
+					break;
+				case 0x11:
+					printf("Ip Protocol Type : UDP\n");
+					eth_ip_udp_packet UDP;
+					unsigned int udp_size = sizeof(eth_ip_udp_packet);
+					convert_raw_to_udp_packet(&UDP,&bufor_eth_ip_udp,sizeof(bufor_eth_ip_udp));
+					print_udp(UDP);
+					break;
+				default:
+					printf("Ip Protocol Type : %02x\n",ip4.protocol);
+			}
+			break;
+		case 0x0806:
+			printf("ARP \n");
+			eth_arp_packet ARP;
+			unsigned int arp_size = sizeof(eth_arp_packet);
+			convert_raw_to_arp_packet(&ARP,&etherhead,arp_size);
+			print_arp(ARP);
+			break;
+		default:
+			break;
+	}
 
 
-
-
-	eth_ip_tcp_packet TCP;
-	unsigned int tcp_size = sizeof(eth_ip_tcp_packet);
-//	memcpy(&TCP,&bufor_eth_ip_tcp,tcp_size);
-//	TCP.ip4.flags.bits = convertShortToBigEndian(TCP.ip4.flags.bits);
-	convert_raw_to_tcp_packet(&TCP,&bufor_eth_ip_tcp,tcp_size);
-	print_tcp(TCP);
-
-
-
+//	eth_arp_packet ARP;
+//	unsigned int arp_size = sizeof(eth_arp_packet);
+//	convert_raw_to_arp_packet(&ARP,&bufor_eth_arp,arp_size);
+//	print_arp(ARP);
+//
+//
+//
+//
+//	eth_ip_icmp_packet ICMP;
+//	unsigned int icmp_size = sizeof(eth_ip_icmp_packet);
+//	convert_raw_to_icmp_packet(&ICMP,&bufor_eth_ip_icmp,icmp_size);
+//	print_icmp(ICMP);
+//
+//
+//
+//	eth_ip_udp_packet UDP;
+//	unsigned int udp_size = sizeof(eth_ip_udp_packet);
+//	convert_raw_to_udp_packet(&UDP,&bufor_eth_ip_udp,sizeof(bufor_eth_ip_udp));
+//	print_udp(UDP);
+//
+//
+//
+//
+//	eth_ip_tcp_packet TCP;
+//	unsigned int tcp_size = sizeof(eth_ip_tcp_packet);
+//	convert_raw_to_tcp_packet(&TCP,&bufor_eth_ip_tcp,tcp_size);
+//	print_tcp(TCP);
+//
+//
+//
 
 
 
